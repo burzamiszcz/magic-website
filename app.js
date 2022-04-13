886bcc2fc3abf62d3758cf1ce8d079a9878220b8
burzamiszcz <burzynskihubert@gmail.com>
2022-04-13 14:28:17 +0200

    animation for photos and recommendations

--- app.js
+++ app.js
@@ -23,3 +23,64 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+
+var slides = document.querySelector('.recommendation .items').children;
+var nextSlide = document.querySelector(".recommendation .items .right-slide");
+var prevSlide = document.querySelector(".recommendation .items .left-slide");
+var totalSlides = slides.length - 1;
+var index = 1
+
+nextSlide.onclick = function () {
+    next("next");
+}
+
+prevSlide.onclick = function () {
+    next("prev");
+}
+
+function next(direction) {
+
+    if (direction=="next"){
+        index++;
+        if (index == totalSlides){
+            index = 1;
+        }
+    }
+    else{
+       if(index == 1){
+           index = totalSlides - 1; 
+       }
+       else{
+           index--;
+       }
+    }
+
+    for (i=1; i < slides.length; i++){
+        slides[i].classList.remove("active");
+    }
+    slides[index].classList.add("active");
+}
+
+
+var photos = document.querySelector('.photos .all-photos').children;
+var allPhotos = photos.length;
+var dispPhoto = 0
+
+
+function change(){
+    dispPhoto ++;
+    if (dispPhoto == allPhotos){
+        dispPhoto = 0;
+    }
+    for (i=0; i < allPhotos; i++){
+        photos[i].classList.remove("active")
+    }
+
+    photos[dispPhoto].classList.add("active")
+
+}
+
+window.onload = function(){
+    setInterval(change, 6000)
+}
